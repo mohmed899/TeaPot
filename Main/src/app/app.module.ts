c0480aa9326a876cae042f4b2313f0bce9e4c37b
Aya Saeed Mostafa Mohammed <ayasmostafa257@gmail.com>
2022-04-14 00:26:45 +0200

    delete main - test
    deleted test and main

--- Main/src/app/app.module.ts
+++ Main/src/app/app.module.ts
@@ -4,16 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
-import { TestComponent } from './Components/test/test.component';
-import { MainComponent } from './Components/main/main.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    TestComponent,
-    MainComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
