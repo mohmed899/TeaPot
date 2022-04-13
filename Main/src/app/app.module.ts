749b39c706a9b01e2db9ef35d39bace1bbc77ed3
Nada_Kohya <94925501+NadaKohya@users.noreply.github.com>
2022-04-13 23:35:21 +0200

    new

--- Main/src/app/app.module.ts
+++ Main/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { TestComponent } from './Components/test/test.component';
+import { MainComponent } from './Components/main/main.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TestComponent
+    TestComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
