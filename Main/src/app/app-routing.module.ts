5b3457a8246c66ff7e24ee370c466908889f8a36
Nada_Kohya <94925501+NadaKohya@users.noreply.github.com>
2022-04-15 21:57:02 +0200

    Features and Footer
    contact component hasn't been finished yet

--- Main/src/app/app-routing.module.ts
+++ Main/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './Components/contact-us/contact-us.component';
+import { FeaturesComponent } from './Components/features/features.component';
 
 const routes: Routes = [
-  
+  {path:"homes/Contact",component:ContactUsComponent}
 ];
 
 @NgModule({
