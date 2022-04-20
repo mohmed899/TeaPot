c73418932a0df2947d89f64816878aa310904bfd
mohmed899 <muhammedkhaled889@gmail.com>
2022-04-20 06:09:04 +0200

    cat item fix

--- Main/src/app/Components/category/category.component.ts
+++ Main/src/app/Components/category/category.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/services/category.service';
-
+declare function aa(message:String): any;
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
-
+ 
   constructor( private Handel:CategoryService ) { }
+
  cats : any 
   ngOnInit(): void {
      this.Handel.getAll().subscribe(
-       (data)=>{this.cats=data},
+       (data)=>{this.cats=data
+      
+      },
        (e)=>{console.log(e)}
     )
   }
