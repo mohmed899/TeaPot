d772bbd47d25329818ab41ab877bfa6627eec57e
mohmed899 <muhammedkhaled889@gmail.com>
2022-04-20 07:31:02 +0200

    fix style

--- Main/src/app/app-routing.module.ts
+++ Main/src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './Components/product/product.component';
 import { HomeComponent } from './Components/home/home.component';
 import { ProductDetailsComponent } from './Components/product-details/product-details.component';
+import { CrudComponent } from './Components/crud/crud.component';
 
 
 const routes: Routes = [
   {path:"",redirectTo:"Home",pathMatch:"full"},
   {path:"Home",component:HomeComponent},
   {path:"Home/Products",component:ProductComponent},
-  {path:"Home/Products/:id",component:ProductDetailsComponent}
-
+  {path:"Home/Products/:id",component:ProductDetailsComponent},
+  {path:"Home/crude",component:CrudComponent}
+  
 ];
 
 @NgModule({
